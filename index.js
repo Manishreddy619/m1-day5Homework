a0be44a47867b38c406175a42ef493c4774e2126
manishreddy619 <manishreddy916@gmail.com>
2021-06-25 18:08:28 +0200

    modified 6 ex

--- index.js
+++ index.js
@@ -108,11 +108,15 @@ console.log(reverseString('strive'));
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
-const upperFirst = (str) => {
-	let upperstr = str.charAt(0).toUpperCase() + str.slice(1);
-	return upperstr;
+const upperFirst = function (str) {
+	const arr = str.split(' ');
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+	}
+	return arr.join(' ');
 };
-console.log(upperFirst('manish'));
+
+console.log(upperFirst('manish reddy elaganti'));
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 9
